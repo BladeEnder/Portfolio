fd5fc34b4f464a9cb2051cfc5ebc8e30cc923011
Ender <enderblade@protonmail.com>
2019-09-21 15:55:10 +0200

    Connexion working

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,6 +6,7 @@
     use Symfony\Component\Form\Extension\Core\Type\SubmitType;
     use Symfony\Component\Form\Extension\Core\Type\TextType;
     use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
     use App\Entity\User;
@@ -19,19 +20,10 @@
         $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
         return $this->render('projects/index.html.twig',array('posts' => $posts));
     }
-
-    /**
-     * @Route("/project/{id}", name="listproject")
-     */
-    public function seeProject($id)
-    {
-        
-        return $this->render('projects/index.html.twig');
-    }
     /**
      * @Route("/project/new", name="addproject")
      */
-  public function addProject(Request $request)
+    public function addProject(Request $request)
     {
         $post = new Post();
         $form = $this->createFormBuilder($post)
@@ -54,12 +46,21 @@
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($post);
             $entityManager->flush();
-            return $this->redirectToRoute('home');
+           // return $this->redirectToRoute('home');
         }
         return $this->render('projects/new.html.twig',array(
             'form' => $form->createView()));
 
 
     }
+    /**
+     * @Route("/project/{id}", name="listproject")
+     */
+    public function seeProject($id)
+    {
+        
+        return $this->render('projects/index.html.twig');
+    }
+    
 }
 ?>
